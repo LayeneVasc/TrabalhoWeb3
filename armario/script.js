8ee54d6a4024bd01292878dd6f9aa8915208a351
Layene <125913267+LayeneVasc@users.noreply.github.com>
2023-04-24 14:39:37 -0300

    Add files via upload

---
+++ armario/script.js
@@ -0,0 +1,36 @@
+function reserva() {
+    document.getElementById("texto").innerHTML = "Situação: Reservado";
+    document.getElementsByName("cor")[0].style.backgroundColor = 'purple';
+
+}
+
+function reserva2() {
+    document.getElementById("texto2").innerHTML = "Situação: Reservado";
+    document.getElementsByName("cor2")[0].style.backgroundColor = 'purple';
+
+}
+
+function reserva3() {
+    document.getElementById("texto3").innerHTML = "Situação: Reservado";
+    document.getElementsByName("cor3")[0].style.backgroundColor = 'purple';
+
+}
+
+function reserva4() {
+    document.getElementById("texto4").innerHTML = "Situação: Reservado";
+    document.getElementsByName("cor4")[0].style.backgroundColor = 'purple';
+
+}
+
+function reserva5() {
+    document.getElementById("texto5").innerHTML = "Situação: Reservado";
+    document.getElementsByName("cor5")[0].style.backgroundColor = 'purple';
+
+}
+
+function reserva6() {
+    document.getElementById("texto6").innerHTML = "Situação: Reservado";
+    document.getElementsByName("cor6")[0].style.backgroundColor = 'purple';
+
+}
+
